349ea84df385249a52c27741976756d2eca0132e
99MingruiZhang <2520098z@student.gla.ac.uk>
2021-07-27 03:24:30 +0100

    commit after the demo of chapter 6 is been finished

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     # return HttpResponse("Rango says hey there partner!")
@@ -14,3 +15,15 @@ def index(request):
 def about(request):
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
