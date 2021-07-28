76683ca0ff3b122073e7032bee9f0471941fc640
99MingruiZhang <2520098z@student.gla.ac.uk>
2021-07-28 23:51:01 +0100

    first commit for chapter 10 after 10.4 been finished

--- rango/views.py
+++ rango/views.py
@@ -22,9 +22,15 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
+    request.session.set_test_cookie()
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
     return render(request, 'rango/about.html')
 
