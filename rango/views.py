4ba1d0bbc14f27d3278d70ed338fc20d0425458f
99MingruiZhang <2520098z@student.gla.ac.uk>
2021-07-28 22:56:23 +0100

    commit after 9.9 finished

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def index(request):
     # return HttpResponse("Rango says hey there partner!")
@@ -129,3 +130,8 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
